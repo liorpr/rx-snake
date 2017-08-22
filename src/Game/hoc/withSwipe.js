ac4b2e1a42bfe67cc910f50a4e00650194ac85a6
Nataly Shrits <nataly@soluto.com>
2017-08-22 16:24:35 +0300

    prevent refresh when swiping down on mobile

--- src/Game/hoc/withSwipe.js
+++ src/Game/hoc/withSwipe.js
@@ -3,10 +3,12 @@ import Swipeable from 'react-swipeable';
 
 export default Component => ({onSwipe, ...props}) => (
   <Swipeable
+    style={{touchAction: 'none'}}
     onSwipedUp={() => onSwipe('SwipedUp')}
     onSwipedRight={() => onSwipe('SwipedRight')}
     onSwipedDown={() => onSwipe('SwipedDown')}
     onSwipedLeft={() => onSwipe('SwipedLeft')}
+    trackMouse
   >
     <Component {...props}/>
   </Swipeable>
