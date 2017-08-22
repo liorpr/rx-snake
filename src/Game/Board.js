17809c2ffbc103596c2514158d33cc0446802ed3
Nataly Shrits <nataly@soluto.com>
2017-08-22 15:48:39 +0300

    fit to screen

--- src/Game/Board.js
+++ src/Game/Board.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { mapProps } from 'recompose';
 import Snake from './Snake';
 import Candy from './Candy';
 import GameState from './utils/GameState';
 import pointShape from './pointShape';
-import LeadBoard from './LeadBoard';
 import './Board.css';
 
-const size = 16;
-
-const Message = ({text}) => (
+const Message = ({ text }) => (
   <div style={{
     top: '50%',
     position: 'relative',
   }}>{text}</div>
 );
 
-const Board = ({ snake, candy, state, width, height, score, current, direction }) => (
+const addSize = mapProps(({ ...props, width, height }) => {
+  const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return { ...props, size: Math.min(16, ~~Math.min(w / width, h / height)) };
+});
+
+const Board = ({ snake, candy, state, width, height, score, current, direction, size }) => (
   <div className="App">
-    <h2>Score: {score}</h2>
     <div className="Board" style={{
       width: width * size,
       height: height * size,
     }}>
-      <Snake shape={snake} size={size} direction={direction} />
-      <Candy point={candy} size={size} />
-      { state === GameState.loaded ? <Message text="Press any arrow to move"/> : null }
-      { state === GameState.ended ? <Message text="GAME ENDED"/> : null }
+      <h2 style={{ opacity: 0.2 }}>Score: {score}</h2>
+      <Snake shape={snake} size={size} direction={direction}/>
+      <Candy point={candy} size={size}/>
+      {state === GameState.loaded ? <Message text="Press any arrow to move"/> : null}
+      {state === GameState.ended ? <Message text="GAME ENDED"/> : null}
     </div>
-    <LeadBoard current={current}/>
   </div>
 );
 
@@ -39,4 +42,4 @@ Board.propTypes = {
   height: PropTypes.number.isRequired,
 };
 
-export default Board;
+export default addSize(Board);
