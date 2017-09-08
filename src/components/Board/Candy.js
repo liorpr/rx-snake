ff6e3c48d490f0f2afb782f1564abafddee308ea
Nataly Shrits <nataly87s@gmail.com>
2017-09-09 01:45:24 +0300

    clearly i'm obsessed

--- src/components/Board/Candy.js
+++ src/components/Board/Candy.js
@@ -2,6 +2,6 @@ import React from 'react';
 import withLocation from '../../hoc/withLocation';
 import candy from '../../resources/candy.png';
 
-const Candy = ({ style }) => <img src={candy} style={style} alt=""/>;
+const Candy = ({ style }) => <img src={candy} style={{...style, zIndex: 3}} alt=""/>;
 
 export default withLocation(Candy);
--- src/hoc/withSwipe.js
+++ src/hoc/withSwipe.js
@@ -1,46 +1,33 @@
-import React from 'react';
+import { lifecycle, mapProps, compose } from 'recompose';
+import R from 'ramda';
 import Hammer from 'hammerjs';
 
-export default Component => class extends React.Component {
-  state = {
-    loaded: false,
-  };
+export default compose(
+  lifecycle({
+    componentDidMount() {
+      const onSwipe = direction => () => {
+        this.props.onSwipe(direction);
+      };
 
-  componentWillUnmount() {
-    if (this.hammer) {
-      this.hammer.stop();
-      this.hammer.destroy();
-    }
-    this.hammer = null;
-  }
-
-  _initHammer = (domElement) => {
-    if (this.state.loaded || !domElement) return;
-    this.hammer = new Hammer(domElement);
-    this.hammer.get('swipe').set({
-      direction: Hammer.DIRECTION_ALL,
-      threshold: 5,
-      velocity: 0.2,
-    });
-    this.hammer.on('swiperight', () => this._onSwipe('SwipeRight'));
-    this.hammer.on('swipeleft', () => this._onSwipe('SwipeLeft'));
-    this.hammer.on('swipeup', () => this._onSwipe('SwipeUp'));
-    this.hammer.on('swipedown', () => this._onSwipe('SwipeDown'));
-    this.hammer.on('doubletap', () => this._onSwipe('Space'));
-    this.setState({ loaded: true });
-  };
-
-  _onSwipe(direction) {
-    const { onSwipe } = this.props;
-    onSwipe(direction);
-  }
-
-  render() {
-    const { onSwipe, ...props } = this.props;
-    return (
-      <div ref={this._initHammer}>
-        <Component {...props} />
-      </div>
-    );
-  }
-}
+      this.hammer = new Hammer(document.documentElement);
+      this.hammer.get('swipe').set({
+        direction: Hammer.DIRECTION_ALL,
+        threshold: 5,
+        velocity: 0.2,
+      });
+      this.hammer.on('swiperight', onSwipe('SwipeRight'));
+      this.hammer.on('swipeleft', onSwipe('SwipeLeft'));
+      this.hammer.on('swipeup', onSwipe('SwipeUp'));
+      this.hammer.on('swipedown', onSwipe('SwipeDown'));
+      this.hammer.on('doubletap', onSwipe('Space'));
+    },
+    componentWillUnmount() {
+      if (this.hammer) {
+        this.hammer.stop();
+        this.hammer.destroy();
+      }
+      this.hammer = null;
+    },
+  }),
+  mapProps(R.dissoc('onSwipe')),
+);
